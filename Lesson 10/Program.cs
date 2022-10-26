26fddf287a147f2cb0c88935fb17459d61ca69ed
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-26 23:11:13 +0300

    Добавьте файлы проекта.

---
+++ Lesson 10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите значение градуса");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение минут");
+            int min_ = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение секунд");
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+
+            Radians radians = new Radians(gradus_, min_, sec_);        
+
+            Console.WriteLine("Переводим градусы в радианы" );
+            double totalradians = radians.ToRadians();
+            Console.WriteLine(totalradians);
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ Lesson 10/Radians.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    class Radians
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    gradus = 0;
+                }
+                else if (value > 360)
+                {
+                    gradus = 360;
+                }
+                else
+                {
+                    {
+                        gradus = value;
+                    }
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    min = 0;
+                }
+                else if (value > 60)
+                {
+                    min = 60;
+                }
+                else
+                {
+                    {
+                        min = value;
+                    }
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    sec = 0;
+                }
+                else if (value > 60)
+                {
+                    sec = 60;
+                }
+                else
+                {
+                    {
+                        sec = value;
+                    }
+                }
+            }
+        }
+
+        public Radians(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return (gradus + min / 60.0 + sec / 3600.0) * (Math.PI) / 180;
+        }
+    }
+}
